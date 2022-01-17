183bbf293872507dbf369d9472c3dada0e969087
MoritzEichhorn <moritz.eichhorn@outlook.com>
2022-01-17 18:39:22 +0100

    Import javafx via maven

---
+++ Client/Desktop/PimpedHOTRoad/src/main/java/at/htl/leonding/pimpedhotroad/desktop/MainWrapper.java
@@ -0,0 +1,11 @@
+package at.htl.leonding.pimpedhotroad.desktop;
+
+/**
+ * Needed for JavaFX to allow a proper application start.
+ * Run the program using this class!
+ */
+public class MainWrapper {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
